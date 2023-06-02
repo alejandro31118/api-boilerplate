fd8ba339bbb245f4bd2faa14106450878f84c3dc
Alejandro Perez <alejandro31118@gmail.com>
2023-06-02 22:35:09 +0100

    First commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import morgan from 'morgan'
+import helmet from 'helmet'
+
+dotenv.config()
+
+const app = express()
+
+const port = process.env.PORT ?? 3000
+const host = process.env.HOST ?? 'localhost'
+const protocol = process.env.PROTOCOL ?? 'http'
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(morgan('common'))
+app.use(helmet())
+
+app.use((_: Request, res: Response) => {
+  res.status(404).send('Resource not found')
+})
+
+app.get('/health', (_: Request, res: Response) => {
+  res.send('<h1>All OK 👌</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`🚀 Server listening at ${protocol}://${host}:${port}`)
+})
